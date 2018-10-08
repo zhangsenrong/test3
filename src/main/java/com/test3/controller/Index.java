0931a6b9c4951c0c9badaec201f83794022d9338
zhangsenrong <zsr1986@189.cn>
2018-10-08 18:05:05 +0800

    first commit

---
+++ src/main/java/com/test3/controller/Index.java
@@ -0,0 +1,10 @@
+package com.test3.controller;
+
+/**
+ * Created by zhangSenRong on 2018/10/8.
+ */
+public class Index {
+    public static void main(String[] args){
+        System.out.println("---------adfa");
+    }
+}
